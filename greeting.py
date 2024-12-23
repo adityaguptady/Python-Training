bb2c8998adf61cd39b134238d62b75c2efa24b27
Aditya Gupta <adityaguptady@gmail.com>
2024-12-23 17:35:03 +0530

    first commit

---
+++ greeting.py
@@ -0,0 +1,35 @@
+# print("Hello world")
+#
+# #This is comment
+#
+# '''This is
+# also an example
+# of multi-line comment'''
+#
+#
+# print("statement 1")
+# print("statement 2")
+# print("statement 3")
+# print("statement 4")
+# print("statement 5")
+#
+# age = 4
+# print(age)
+# age = "Four"
+# print(age)
+
+# name = "Aditya"
+# profession = "Software developer"
+# experience = 10
+# print("Hello, I am",name,". I am a ", profession, " professionally. And I have around ", experience, " with it!")
+
+# name = input("Please enter your name: ")
+# print("Hello ", name)
+
+# x = input("Enter first value for sum: ")
+# y = input("Enter second value for sum: ")
+# z = int(x) + int(y)
+# print("Sum: ",z)
+
+x = 5
+print("Type of x is: ", type(x))
---
+++ test.py
@@ -0,0 +1,13 @@
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+
+age = 21
+#age is integer
+print("My Age is: ",age)
+
+age = "Twenty one"
+# age is now a String
+print("My Age is: ",age)
